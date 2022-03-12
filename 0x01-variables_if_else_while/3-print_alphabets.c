f9c0abe6b01c311f7b88e9ebbe64c5387b512eae
Whiney96 <Uziwinnie@gmail.com>
2022-03-11 17:14:55 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0 for success
  */
+
 int main(void)
 {
 	int letter;
-	
+
 	/* Print alphabet in lowercase */
 	letter = 'a';
 	while (letter <= 'z')
-
 	{
 		putchar(letter);
 		letter++;
@@ -21,7 +21,6 @@ int main(void)
 	/* Print alphabet in uppercase */
 	letter = 'A';
 	while (letter <= 'Z')
-
 	{
 		putchar(letter);
 		letter++;
