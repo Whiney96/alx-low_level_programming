ecdcc80d91973af16d8a629bf3cc79d7fcc3f4b0
Whiney96 <Uziwinnie@gmail.com>
2022-03-15 16:38:51 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * print_alphabet - check the code for Holberton School students.
+ * Description: Prints the alphabet with _putcha
  * Return: Always 0 for success
  */
 
 void print_alphabet(void)
-/* void indicates no return expected */
 {
 	char c;
 
