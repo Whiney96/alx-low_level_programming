9d8efe2c871a240a22050ae5ee366b05e3350dd5
Whiney96 <Uziwinnie@gmail.com>
2022-03-16 07:09:01 -0700

    task 12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- *print_to_98 - count from n to 98.
- @n:integer.
-Return: Always 0.
+* print_to_98 - count from n to 98.
+* @n:integer.
+* Return: Always 0.
 */
 void print_to_98(int n)
 {
