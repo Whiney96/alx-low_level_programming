178739b06a505709fb62e8bbf3fb898ca0a91c80
Whiney96 <Uziwinnie@gmail.com>
2022-03-16 06:19:26 -0700

    task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,18 @@
 #include "main.h"
 
 /**
- * print_sign - check sign.
- * @n: integer.
- * Return: Always 0.
+ * _isalpha - check charachter.
+ * @c: charachter
+ *  Return: Always 0.
  */
-int print_sign(int n)
+int _isalpha(int c)
 {
-	if (n > 0)
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
 	{
-		_putchar('+');
 		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		return (0);
 	}
 }
