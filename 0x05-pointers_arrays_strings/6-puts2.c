8b3e8431134a0cedc1587f786eed45fcbe00e90f
Whiney96 <Uziwinnie@gmail.com>
2022-03-22 15:46:54 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,9 +13,9 @@ void puts2(char *str)
 	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
-			if ((i % 2) == 0)
-			else
-				continue;
+			_putchar(str[i]);
+		else
+			continue;
 	}
 	_putchar('\n');
 }
