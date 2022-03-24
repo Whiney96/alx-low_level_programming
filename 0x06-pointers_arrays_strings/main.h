fb4d91d7284d3480856dddc608f1d4d9b5efe4bb
Whiney96 <Uziwinnie@gmail.com>
2022-03-24 06:41:29 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-	
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 char *_strcat(char *dest, char *src);
 
 void print_number(int n);
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
-void reverse_array(int *a, int n;)
+void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
 
