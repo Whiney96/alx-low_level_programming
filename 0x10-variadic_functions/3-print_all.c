332807da70e46c0c59d78ce55e4eceed016d6208
Whiney96 <Uziwinnie@gmail.com>
2022-04-14 08:46:07 -0700

    task 3 edited

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,7 @@
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
+ *
  * Return: no return.
  */
 void print_all(const char * const format, ...)
@@ -22,7 +23,7 @@ void print_all(const char * const format, ...)
 			{
 				printf(", ");
 				break;
-			} j++
+			} j++;
 		}
 		switch (format[i])
 		{
