60323030e4988f3424f0b23a7e68c1b819b5ce72
Whiney96 <Uziwinnie@gmail.com>
2022-04-21 08:29:40 -0700

    README and lists.h upload

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
